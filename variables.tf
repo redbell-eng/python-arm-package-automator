024359bd89faf531a3d43a2e919cc44ed528dd98
Han-Lon <josephmaxmorris7@gmail.com>
2022-08-10 13:15:09 -0500

    Remove unnecessary aws profile variable

--- variables.tf
+++ variables.tf
@@ -8,8 +8,3 @@ variable "environment" {
     error_message = "Please use dev, test, or prod for environment."
   }
 }
-
-variable "aws_profile" {
-  type = string
-  description = "The name of the AWS profile to use when authenticating during Packer and Terraform builds."
-}
